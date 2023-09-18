bf8d9d50cf9c7f23182e78f4e4d38359d8a276f9
Felipe Telles <felipe.melo.telles@gmail.com>
2023-09-18 17:09:04 -0300

    added interrupt

--- Core/Src/Controllers/SystemController.c
+++ Core/Src/Controllers/SystemController.c
@@ -1,17 +1,15 @@
 #include "Controllers/SystemController.h"
 
-#include <stdint.h>
-
 #include "main.h"
+
+#include <stdint.h>
 
 void SystemController_Init(void) {}
 
 void SystemController_Run(void) {}
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-    if (GPIO_Pin == B1_Pin)
-    {
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+    if (GPIO_Pin == B1_Pin) {
         HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
     }
 }
