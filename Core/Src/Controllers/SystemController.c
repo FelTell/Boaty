32021ed6eefff433fbf376bb42c7b09aa7425bc4
Felipe Telles <felipe.melo.telles@gmail.com>
2023-10-11 23:44:32 -0300

    working rudder

--- Core/Src/Controllers/SystemController.c
+++ Core/Src/Controllers/SystemController.c
@@ -4,23 +4,44 @@
 #include <stdint.h>
 
 #include "Services/PowerControlDriver.h"
+#include "Services/RudderControlDriver.h"
 
 #include "main.h"
 
-static volatile uint32_t value = 0;
+static volatile uint32_t powerValue  = 0;
+static volatile uint32_t rudderAngle = 0;
 
 void SystemController_Init(void) {
     PowerControlDriver_Init();
+    RudderControlDriver_Init();
 }
 
-void SystemController_Run(void) {}
+void SystemController_Run(void) {
+    RudderControlDriver_SetAngle(-90);
+    HAL_Delay(1000);
+    PowerControlDriver_SetPower(10,false);
+    HAL_Delay(1000);
+    RudderControlDriver_SetAngle(0);
+    HAL_Delay(2000);
+    RudderControlDriver_SetAngle(90);
+    HAL_Delay(1000);
+    PowerControlDriver_SetPower(80,false);
+    HAL_Delay(1000);
+    RudderControlDriver_SetAngle(0);
+    HAL_Delay(2000);
+}
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
     switch (GPIO_Pin) {
         case B1_Pin: {
-            PowerControlDriver_SetPower(value += 10, false);
-            if (value > 100) {
-                value = 0;
+            PowerControlDriver_SetPower(powerValue += 10,
+                                        false);
+            if (powerValue > 100) {
+                powerValue = 0;
+            }
+            RudderControlDriver_SetAngle(rudderAngle += 10);
+            if (rudderAngle > 90) {
+                rudderAngle = -90;
             }
         } break;
     }
--- Core/Src/Services/RudderControlDriver.c
+++ Core/Src/Services/RudderControlDriver.c
@@ -8,6 +8,13 @@
 #define MAX_ANGLE 90
 #define MIN_ANGLE (-90)
 
+#define MIN_PWM_VALUE     35
+#define MAX_PWM_VALUE     115
+#define NEUTRAL_PWM_VALUE 75
+
+#define ANGLE_TO_PWM_INTERCEPT 75
+#define ANGLE_TO_PWM_SLOPE     (4.f / 9.f)
+
 static bool initDone;
 
 void RudderControlDriver_Init(void) {
@@ -15,6 +22,8 @@ void RudderControlDriver_Init(void) {
         return;
     }
     Pwm3Driver_Init();
+
+    RudderControlDriver_SetAngle(0);
 
     initDone = true;
 }
@@ -26,5 +35,9 @@ void RudderControlDriver_SetAngle(int32_t angle) {
 
     UTILS_CLAMP(angle, MIN_ANGLE, MAX_ANGLE);
 
-    Pwm3Driver_SetValue(PWM3_CHANNEL_RUDDER, angle * 10);
+    const int32_t pwmValue =
+        (int32_t)((float)angle * ANGLE_TO_PWM_SLOPE +
+                  ANGLE_TO_PWM_INTERCEPT);
+
+    Pwm3Driver_SetValue(PWM3_CHANNEL_RUDDER, pwmValue);
 }
