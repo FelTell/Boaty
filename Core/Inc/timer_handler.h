c3948bfec52160236e857debd7d5da79ba95e8d4
Felipe Telles <felipe.melo.telles@gmail.com>
2023-11-27 01:07:36 -0300

    added timer

---
+++ Core/Inc/timer_handler.h
@@ -0,0 +1,33 @@
+/**
+ * @file timer_handler.h
+ * @author Felipe Telles (felipe.telles@42we.tech)
+ * @brief timer handler to allow a simple scaler
+ * @date 27-11-2023
+ *
+ *
+ */
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/**
+ * @brief Waits a specific timer to be elapsed by a desired
+ * amount. Allows multiple timers by sending a different
+ * timer_start.
+ *
+ * @param timer_start Timer specifier.
+ * @param delay Amount desired to wait, max is 2^32, which
+ * is equal to approximately 50 days. Values higher than
+ * that will not work as expected!
+ * @return true The desired time has elapsed.
+ * @return false The desired time has not been elapsed.
+ */
+bool timer_wait_ms(uint32_t timer_start, uint32_t delay);
+/**
+ * @brief Restart timer to current tick value.
+ *
+ * @param timer_to_restart Address of timer specifier.
+ */
+void timer_restart(uint32_t* timer_to_restart);
